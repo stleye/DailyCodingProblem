ec6005d50929fbc7641866377629b34deb1608ee
Sebastian Tleye <stleye@gmail.com>
2019-04-02 11:26:49 -0300

    Initial Commit

---
+++ DailyCodingProblem/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DailyCodingProblem
+//
+//  Created by Sebastian Tleye on 02/04/2019.
+//  Copyright © 2019 Humileants. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
