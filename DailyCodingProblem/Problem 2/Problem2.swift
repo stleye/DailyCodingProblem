b72e02227f43cba56d3af09875bb2622768fe17d
Sebastian Tleye <stleye@gmail.com>
2019-04-12 20:53:19 -0300

    Improved problem 2, Complexity from O(n^2) to O(n)

--- DailyCodingProblem/Problem 2/Problem2.swift
+++ DailyCodingProblem/Problem 2/Problem2.swift
@@ -28,16 +28,37 @@ class Problem2 {
         return v.map({ $0 == 0 ? productOfAll : (zeros == 1 ? 0 : productOfAll / $0) })
     }
 
-    // Time complexity: O(N^2)
+    // Time complexity: O(N)
     // Space complexity: O(N)
     static func solveWithoutDivision(_ v: [Int]) -> [Int] {
+        if v.isEmpty { return [] }
+        if v.count == 1 { return [1] }
+        var prefixes = Array.init(repeating: 1, count: v.count)
+        var suffixes = Array.init(repeating: 1, count: v.count)
+        
+        var product = v[0]
+        prefixes[0] = v[0]
+        for index in 1..<v.count {
+            prefixes[index] = product * v[index]
+            product = prefixes[index]
+        }
+        product = v.last!
+        suffixes[v.count-1] = product
+        for index in (0..<v.count-1).reversed() {
+            suffixes[index] = product * v[index]
+            product = suffixes[index]
+        }
         var result = Array(repeating: 0, count: v.count)
-        for i in 0..<v.count {
-            var product = 1
-            for j in 0..<v.count where j != i {
-                product *= v[j]
+        for index in 0..<v.count {
+            if index-1 >= 0 {
+                if index+1 < v.count {
+                    result[index] = prefixes[index-1] * suffixes[index+1]
+                } else {
+                    result[index] = prefixes[index-1]
+                }
+            } else {
+                result[index] = suffixes[index+1]
             }
-            result[i] = product
         }
         return result
     }
